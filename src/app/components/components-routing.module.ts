4f1f8fb226f8444a1ccfc9f063300e334fef7acd
sngrisa <sngrisa@gmail.com>
2023-09-09 17:29:22 -0300

    Mejorando recetas

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecetasComponent } from './recetas/recetas.component';
+import { DetallesRecetaComponent } from './recetas/detalles-receta/detalles-receta.component';
 
 const routes: Routes = [
   { path: '', component: RecetasComponent },
+  { path: 'detallesReceta', component: DetallesRecetaComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
