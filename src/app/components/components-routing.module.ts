6b6f11206622bbf2a0d3ce048ab1b43d32336385
gonzalogrisafi <gonzagrisa@gmail.com>
2023-09-09 16:56:19 -0300

    deleted unused componentes

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PastafrolasComponent } from './recetas/pastafrolas/pastafrolas.component';
-import { BiznaranjaComponent } from './recetas/biznaranja/biznaranja.component';
-import { LemonpaeComponent } from './recetas/lemonpae/lemonpae.component';
-import { BizchocolateComponent } from './recetas/bizchocolate/bizchocolate.component';
 import { RecetasComponent } from './recetas/recetas.component';
 
 const routes: Routes = [
   { path: '', component: RecetasComponent },
-  { path: 'pastafrola', component: PastafrolasComponent },
-  { path: 'biznaranja', component: BiznaranjaComponent },
-  { path: 'lemonpae', component: LemonpaeComponent },
-  { path: 'bizchocolate', component: BizchocolateComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ImgComponent } from './home/img/img.component';
-import { BizchocolateComponent } from './recetas/bizchocolate/bizchocolate.component';
-import { BiznaranjaComponent } from './recetas/biznaranja/biznaranja.component';
-import { LemonpaeComponent } from './recetas/lemonpae/lemonpae.component';
-import { PastafrolasComponent } from './recetas/pastafrolas/pastafrolas.component';
 import { RecetasComponent } from './recetas/recetas.component';
 import { ComponentsRoutingModule } from './components-routing.module';
 import { CardsRecetasComponent } from './recetas/cards-recetas/cards-recetas.component';
@@ -16,10 +12,6 @@ import { CardsRecetasComponent } from './recetas/cards-recetas/cards-recetas.com
   declarations: [
     HomeComponent,
     ImgComponent,
-    BizchocolateComponent,
-    BiznaranjaComponent,
-    LemonpaeComponent,
-    PastafrolasComponent,
     RecetasComponent,
     CardsRecetasComponent,
   ],
@@ -30,10 +22,6 @@ import { CardsRecetasComponent } from './recetas/cards-recetas/cards-recetas.com
   exports: [
     HomeComponent,
     ImgComponent,
-    BizchocolateComponent,
-    BiznaranjaComponent,
-    LemonpaeComponent,
-    PastafrolasComponent,
     RecetasComponent,
     ComponentsRoutingModule
   ]
